83501e3c92f60000dc69c2fdb72a10dd321edc3c
josedejesusAmaya <pp.jjag@gmail.com>
2020-05-20 00:15:19 -0500

    first observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import { Observable } from 'rxjs';
 
+// const obs$ = Observable.create();
 
-console.log('Hola Mundo!');
+const obs$ = new Observable<string>( subs => {
+    subs.next('Hola Pepe');
+    subs.complete();
+});
 
-
-
-
-
-
-
+obs$.subscribe(console.log);
